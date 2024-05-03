a74b1db5c923c3395b965b2c41ee31b6d886d94f
leandro.khalil <leandro.khalil@inventsoftware.info>
2024-05-03 15:44:20 -0300

    Add project files.

---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiApp1
+{
+    public static class MauiProgram
+    {
+        /// <summary>
+        /// https://youtu.be/3OQSEO2Ubus?t=268
+        /// </summary>
+        /// <returns></returns>
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
