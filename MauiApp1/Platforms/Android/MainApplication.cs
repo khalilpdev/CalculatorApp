6bcb7db56ecc5b2a985754a16c70cb5de7b57efd
leandro.khalil <leandro.khalil@inventsoftware.info>
2024-05-06 18:28:13 -0300

    remove underline in android

--- MauiApp1/Platforms/Android/MainApplication.cs
+++ MauiApp1/Platforms/Android/MainApplication.cs
@@ -1,4 +1,5 @@
 ﻿using Android.App;
+using Android.Content.Res;
 using Android.Runtime;
 
 namespace MauiApp1
@@ -9,8 +10,19 @@ namespace MauiApp1
         public MainApplication(IntPtr handle, JniHandleOwnership ownership)
             : base(handle, ownership)
         {
+            Microsoft.Maui.Handlers.EntryHandler.Mapper.AppendToMapping(nameof(Entry), (handler, view) => 
+            {
+                if (view is  Entry) 
+                { 
+                    // Remove Underline
+                    handler.PlatformView.BackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.Transparent);
+
+                    handler.PlatformView.ShowSoftInputOnFocus = false;
+                }
+            });
         }
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
     }
 }
+roid
